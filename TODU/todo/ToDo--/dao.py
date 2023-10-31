1261e3794071cdcb4cf73dd3ddf578cf5c5670e0
fantinelli <140507103+fantinelli@users.noreply.github.com>
2023-10-31 10:49:24 -0300

    Update dao.py

--- TODU/todo/ToDo--/dao.py
+++ TODU/todo/ToDo--/dao.py
@@ -1,23 +1,51 @@
+from random import *
+
+Arquivo = "ToDo.txt"
+
+
 class DaoAdicionarTarefa():
     def __init__(self, tarefa):
-        with open("tarefas.txt", "w") as arquivo:
-            arquivo.write(tarefa + "\n")
-            print("Tarefa adicionada ao DAO.")
+        self.tarefa = tarefa
+        self.status = "A"
 
-class DaoExcluirTarefa():
-    def __init__(self, excluir):
-         with open("tarefas.txt", "r") as arquivo:
-            linhas = arquivo.readlines()
-            linhas.pop(int(excluir) - 1)
-            with open("tarefas.txt", "w") as arquivo:
-                arquivo.writelines(linhas)
-                print("Tarefa removida do DAO.")
+    def adicionar(self, x):
+        with open(Arquivo, "a") as arquivo:
+            tarefa_formatada = f"{self.status}\t\t\t{x}\t{self.tarefa}"
+            
+            with open(Arquivo, "r") as arquivo:
+                ler = arquivo.read()
+
+            with open(Arquivo, "a") as arquivo:
+                if "STATUS: \tID: \tTAREFA:" not in ler:
+                    arquivo.write(f"STATUS: \tID: \tTAREFA:\n")
+                arquivo.write(f"{tarefa_formatada}\n")
+                return True
+
 
 class DaoListarTarefa():
-    def __init__(self):
-        with open("tarefas.txt", "r") as arquivo:
+    def listar(self):
+        with open(Arquivo,"r") as arquivo:
             linhas = arquivo.readlines()
-            cont = 0
-            for tarefas in linhas:
-                cont += 1
-                print(f"{cont}. {tarefas}")
+        return linhas
+    
+class DaoAlterar():
+    def alterarTarefa(self, tarefa_A, nova_tarefa):
+        with open(Arquivo, "r") as arquivo:
+            texto = arquivo.read()
+
+            textoNovo = texto.replace(tarefa_A, nova_tarefa)
+
+            with open(Arquivo, "w") as arquivo:
+                arquivo.write(textoNovo)
+                return True
+            
+class DAO_ConcluirExcluirTarefa:
+    def ConcluirExcluirTarefa(self, statusA, statusN):
+        with open(Arquivo, "r") as arquivo:
+            status = arquivo.read()
+
+        statusNovo = status.replace(statusA, statusN)
+
+        with open(Arquivo, "w") as arquivo:
+            arquivo.write(statusNovo)
+            return True
